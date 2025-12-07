7f0d7e96305d6077a264dbf59b17888942003705
Polina785643 <polytutynina@gmail.com>
2025-12-08 00:31:43 +0500

    Переделаны теперь просто шаблонные функци

--- 02_week/tasks/swap_ptr/swap_ptr.cpp
+++ 02_week/tasks/swap_ptr/swap_ptr.cpp
@@ -1,15 +1,19 @@
- #include <stdexcept>
+#include <stdexcept>
 
-/*Функция обмена указателей*/
-
-template<typename T>
-void SwapPtr(T*& ptr1, T*& ptr2) {
-    T* temp = ptr1;
+void SwapPtr(int*& ptr1, int*& ptr2) {
+    int* temp = ptr1;
     ptr1 = ptr2;
     ptr2 = temp;
 }
 
-// Явная инстанциация для нужных типов
-template void SwapPtr<int>(int*&, int*&);
-template void SwapPtr<const int>(const int*&, const int*&);
-template void SwapPtr<int*>(int**&, int**&);
+void SwapPtr(const int*& ptr1, const int*& ptr2) {
+    const int* temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
+}
+
+void SwapPtr(int**& ptr1, int**& ptr2) {
+    int** temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
+}
