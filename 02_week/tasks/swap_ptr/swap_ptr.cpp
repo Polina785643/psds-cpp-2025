4deb0c1e6044afc389334140034201466b33b9f2
Polina785643 <polytutynina@gmail.com>
2025-12-08 00:12:57 +0500

    Добавление шаблонных функций

--- 02_week/tasks/swap_ptr/swap_ptr.cpp
+++ 02_week/tasks/swap_ptr/swap_ptr.cpp
@@ -8,3 +8,8 @@ void SwapPtr(T*& ptr1, T*& ptr2) {
     ptr1 = ptr2;
     ptr2 = temp;
 }
+
+// Явная инстанциация для нужных типов
+template void SwapPtr<int>(int*&, int*&);
+template void SwapPtr<const int>(const int*&, const int*&);
+template void SwapPtr<int*>(int**&, int**&);
