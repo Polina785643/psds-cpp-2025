04be9041e8c02d71e0b774471e6f802dd0d8e7cd
Polina <polytutynina@gmail.com>
2026-03-06 21:05:30 +0000

    Добавлена функция make_unique

--- 07_week/tasks/make_unique/make_unique.cpp
+++ 07_week/tasks/make_unique/make_unique.cpp
@@ -1,4 +1,8 @@
 #include <memory>
+#include <utility>  // для std::forward
 
-
-/* return_type */ MakeUnique( /* args */ );
+// Создает unique_ptr на объект типа T, передавая аргументы в его конструктор
+template<typename T, typename... Args>
+std::unique_ptr<T> MakeUnique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
